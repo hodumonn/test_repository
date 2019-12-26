a2691feef60360409706bba5be65989a82ca8f89
hjseo <hjseo@DESKTOP-FLMGSJF>
2019-12-26 15:03:40 +0900

    create Car Class

---
+++ test/src/main/java/com/company/test/VO/Car.java
@@ -0,0 +1,63 @@
+package com.company.test.VO;
+
+import java.util.Date;
+
+public class Car {
+	private int car_key;
+	private String car_index;
+	private String car_name;
+	private char delete_yn;
+	private Date reg_time;
+	private Date update_time;
+	private String config_data;
+	
+	public int getCar_key() {
+		return car_key;
+	}
+	public void setCar_key(int car_key) {
+		this.car_key = car_key;
+	}
+	public String getCar_index() {
+		return car_index;
+	}
+	public void setCar_index(String car_index) {
+		this.car_index = car_index;
+	}
+	public String getCar_name() {
+		return car_name;
+	}
+	public void setCar_name(String car_name) {
+		this.car_name = car_name;
+	}
+	public char getDelete_yn() {
+		return delete_yn;
+	}
+	public void setDelete_yn(char delete_yn) {
+		this.delete_yn = delete_yn;
+	}
+	public Date getReg_time() {
+		return reg_time;
+	}
+	public void setReg_time(Date reg_time) {
+		this.reg_time = reg_time;
+	}
+	public Date getUpdate_time() {
+		return update_time;
+	}
+	public void setUpdate_time(Date update_time) {
+		this.update_time = update_time;
+	}
+	public String getConfig_data() {
+		return config_data;
+	}
+	public void setConfig_data(String config_data) {
+		this.config_data = config_data;
+	}
+	
+	@Override
+	public String toString() {
+		return "Car [car_key=" + car_key + ", car_index=" + car_index + ", car_name=" + car_name + ", delete_yn="
+				+ delete_yn + ", reg_time=" + reg_time + ", update_time=" + update_time + ", config_data=" + config_data
+				+ "]";
+	}
+}
